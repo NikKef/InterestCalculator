0174eb137797de008e654fa61aebcf58014e52da
Nikolas Kefalonitis <nikolaskefalonitis@gmail.com>
2025-09-16 11:47:33 +0300

    Create a basic menu for the app

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+void menu();
+
 int main()
 {
-    cout << "Hello World!\n";
+    
 
     return 0;
+}
+
+void menu()
+{
+    cout << "    ---------------------------------\n";
+    cout << "  -------------------------------------\n";
+    cout << "---------- Interest Calculator ----------\n";
+    cout << "  -------------------------------------\n";
+    cout << "    ---------------------------------\n";
+    cout << "1. Find out which age you can retire using your current investing practises\n";
+    cout << "2. Find out how to change your stategy to retire at your desired age\n";
+    cout << "3. Exit\n";
 }
