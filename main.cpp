cc9eb17376f556cee7660f8a6cbf2f7878692c4d
Nikolas Kefalonitis <nikolaskefalonitis@gmail.com>
2025-09-16 11:54:40 +0300

    update menu and add basic loop

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
+//main menu
 void menu();
 
 int main()
 {
+    int choice = -1;
+
+    do
+    {
+        menu();
+        cin >> choice;
+    } while (choice != 3);
     
 
     return 0;
@@ -13,12 +21,13 @@ int main()
 
 void menu()
 {
-    cout << "    ---------------------------------\n";
-    cout << "  -------------------------------------\n";
-    cout << "---------- Interest Calculator ----------\n";
-    cout << "  -------------------------------------\n";
-    cout << "    ---------------------------------\n";
+    cout << "                   ---------------------------------\n";
+    cout << "                -------------------------------------\n";
+    cout << "              ---------- Interest Calculator ----------\n";
+    cout << "                -------------------------------------\n";
+    cout << "                  ---------------------------------\n";
     cout << "1. Find out which age you can retire using your current investing practises\n";
     cout << "2. Find out how to change your stategy to retire at your desired age\n";
     cout << "3. Exit\n";
+    cout << "Enter(1-3): ";
 }
